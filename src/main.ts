eea1d6be0c1b3bcfa00c03e1db0897373cb8fa6d
Sidharth Sreekumar <sidarth.sreekumar@gmail.com>
2025-07-06 15:15:06 -0600

    Add role enum

--- src/main.ts
+++ src/main.ts
@@ -21,9 +21,17 @@ declare global {
   }
 
   interface CreepMemory {
-    role: string;
+    role: MemoryRole;
+    building?: boolean;
+    upgrading?: boolean;
     room?: string;
     working?: boolean;
+  }
+
+  enum MemoryRole {
+    HARVESTER = "harvester",
+    UPGRADER = "upgrader",
+    BUILDER = "builder"
   }
 
   // Syntax for adding proprties to `global` (ex "global.log")
@@ -37,74 +45,69 @@ declare global {
 // When compiling TS to JS and bundling with rollup, the line numbers and file names in error messages change
 // This utility uses source maps to get the line numbers and file names of the original, TS source code
 export const loop = ErrorMapper.wrapLoop(() => {
-  console.log(`Current game tick is ${Game.time}`);
-
-  // Automatically delete memory of missing creeps
   for (const name in Memory.creeps) {
-    for (const name in Memory.creeps) {
-      if (!Game.creeps[name]) {
-        delete Memory.creeps[name];
-        console.log("Clearing non-existing creep memory:", name);
-      }
+    if (!Game.creeps[name]) {
+      delete Memory.creeps[name];
+      console.log("Clearing non-existing creep memory:", name);
+    }
+  }
+
+  const tower = Game.getObjectById("TOWER_ID" as Id<_HasId>) as StructureTower;
+  if (tower) {
+    const closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+      filter: structure => structure.hits < structure.hitsMax
+    });
+    if (closestDamagedStructure) {
+      tower.repair(closestDamagedStructure);
     }
 
-    const tower = Game.getObjectById("TOWER_ID" as Id<_HasId>) as StructureTower;
-    if (tower) {
-      const closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
-        filter: structure => structure.hits < structure.hitsMax
-      });
-      if (closestDamagedStructure) {
-        tower.repair(closestDamagedStructure);
-      }
-
-      const closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-      if (closestHostile) {
-        tower.attack(closestHostile);
-      }
+    const closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+    if (closestHostile) {
+      tower.attack(closestHostile);
     }
+  }
 
-    const harvesters = _.filter(Game.creeps, (creep: Creep) => creep.memory.role == 'harvester');
-    // console.log('Harvesters: ' + harvesters.length);
-    const builders = _.filter(Game.creeps, (creep: Creep) => creep.memory.role == 'builder');
-    // console.log('Builders: ' + builders.length);
-    const upgraders = _.filter(Game.creeps, (creep: Creep) => creep.memory.role == 'upgrader');
-    // console.log('Upgraders: ' + upgraders.length);
+  const harvesters = _.filter(Game.creeps, (creep: Creep) => creep.memory.role == MemoryRole.HARVESTER);
+  // console.log('Harvesters: ' + harvesters.length);
+  const builders = _.filter(Game.creeps, (creep: Creep) => creep.memory.role == MemoryRole.BUILDER);
+  // console.log('Builders: ' + builders.length);
+  const upgraders = _.filter(Game.creeps, (creep: Creep) => creep.memory.role == MemoryRole.UPGRADER);
+  // console.log('Upgraders: ' + upgraders.length);
 
-    if (harvesters.length < 2) {
-      const newName = "Harvester" + Game.time;
-      console.log("Spawning new harvester: " + newName);
-      Game.spawns["Spawn1"].spawnCreep([WORK, CARRY, MOVE], newName, { memory: { role: "harvester" } });
-    } else if (builders.length < 1) {
-      const newName = "Builder" + Game.time;
-      console.log("Spawning new builder: " + newName);
-      Game.spawns["Spawn1"].spawnCreep([WORK, CARRY, MOVE], newName, { memory: { role: 'builder' } });
-    } else if (upgraders.length < 5) {
-      const newName = "Upgrader" + Game.time;
-      console.log("Spawning new upgrader: " + newName);
-      Game.spawns["Spawn1"].spawnCreep([WORK, CARRY, MOVE], newName, { memory: { role: 'upgrader' } });
+  if (harvesters.length < 2) {
+    const newName = "Harvester" + Game.time;
+    console.log("Spawning new harvester: " + newName);
+    Game.spawns["Spawn1"].spawnCreep([WORK, CARRY, MOVE], newName, { memory: { role: MemoryRole.HARVESTER } });
+  } else if (builders.length < 1) {
+    const newName = "Builder" + Game.time;
+    console.log("Spawning new builder: " + newName);
+    Game.spawns["Spawn1"].spawnCreep([WORK, CARRY, MOVE], newName, { memory: { role: MemoryRole.BUILDER } });
+  } else if (upgraders.length < 5) {
+    const newName = "Upgrader" + Game.time;
+    console.log("Spawning new upgrader: " + newName);
+    Game.spawns["Spawn1"].spawnCreep([WORK, CARRY, MOVE], newName, { memory: { role: MemoryRole.UPGRADER } });
+  }
+
+  if (Game.spawns["Spawn1"].spawning) {
+    const spawningCreep = Game.creeps[Game.spawns["Spawn1"].spawning.name];
+    Game.spawns["Spawn1"].room.visual.text(
+      "🛠️" + spawningCreep.memory.role,
+      Game.spawns["Spawn1"].pos.x + 1,
+      Game.spawns["Spawn1"].pos.y,
+      { align: "left", opacity: 0.8 }
+    );
+  }
+
+  for (const name in Game.creeps) {
+    const creep = Game.creeps[name];
+    if (creep.memory.role === MemoryRole.HARVESTER) {
+      roleHarvester.run(creep);
     }
-
-    if (Game.spawns["Spawn1"].spawning) {
-      const spawningCreep = Game.creeps[Game.spawns["Spawn1"].spawning.name];
-      Game.spawns["Spawn1"].room.visual.text(
-        "🛠️" + spawningCreep.memory.role,
-        Game.spawns["Spawn1"].pos.x + 1,
-        Game.spawns["Spawn1"].pos.y,
-        { align: "left", opacity: 0.8 }
-      );
+    if (creep.memory.role === MemoryRole.BUILDER) {
+      roleBuilder.run(creep);
     }
-
-    for (const name in Game.creeps) {
-      const creep = Game.creeps[name];
-      if (creep.memory.role === "harvester") {
-        roleHarvester.run(creep);
-      }
-      if (creep.memory.role === "builder") {
-        roleBuilder.run(creep);
-      }
-      if (creep.memory.role === "upgrader") {
-        roleUpgrader.run(creep);
-      }
+    if (creep.memory.role === MemoryRole.UPGRADER) {
+      roleUpgrader.run(creep);
     }
   }
 });
